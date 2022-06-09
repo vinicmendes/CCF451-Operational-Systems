ddbd3f94ffe44295651a12d9bbfb5bfe747f7020
vinicmendes <vinicius.o.mendes@ufv.br>
2022-06-09 09:08:34 -0300

    add instrucao

--- src/estruturas/instrucao.h
+++ src/estruturas/instrucao.h
@@ -1,6 +1,11 @@
 #ifndef INSTRUCAO_H
 #define INSTRUCAO_H
 
-
+typedef struct Instrucao{
+    char id; // Id da instrução. Ex: N, D, V, A, etc.
+    int var1;
+    int var2;
+    char arqv[20];
+} instrucao;
 
 #endif
