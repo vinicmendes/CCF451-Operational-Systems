46a8743ae37801364354e4322f4033dd194aabf7
vinicmendes <vinicius.o.mendes@ufv.br>
2022-06-15 19:51:04 -0300

    up

--- src/estruturas/pipe.h
+++ src/estruturas/pipe.h
@@ -1,13 +1,12 @@
 #ifndef PIPE_H
 #define PIPE_H
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <stdbool.h>
-
-#include "fork.h"
 
 typedef struct Pipe{
     int fd[2];
