f6214af17d4e2419978d988f3565954014bb0b9b
vinicmendes <vinicius.o.mendes@ufv.br>
2022-06-17 16:22:15 -0300

    up

--- src/estruturas/leitor.c
+++ src/estruturas/leitor.c
@@ -31,9 +31,40 @@ void leArquivoInstrucao(instrucao **inst, char *nomeArquivo){
     FILE *file;
     char c;
     char arqv[20];
+    int ordemInstrucao = 0;
     (*inst) = (instrucao*)malloc((100)*sizeof(instrucao));
     file = fopen(nomeArquivo, "r");
-    
+    if(file == NULL){
+        printf("Erro ao abrir arquivo\n");
+        exit(1);
+    }
+    while((c=fgetc(file))!= EOF){
+        c = fgetc(file);
+        switch (c)
+        {
+        case 'N':
+            fscanf(file, "%d", (*inst)[ordemInstrucao].var1);
+        case 'D':
+            fscanf(file, "%d", (*inst)[ordemInstrucao].var1);
+        case 'V':
+            fscanf(file, "%d %d", (*inst)[ordemInstrucao].var1, (*inst)[ordemInstrucao].var2);
+        case 'A':
+            fscanf(file, "%d %d", (*inst)[ordemInstrucao].var1, (*inst)[ordemInstrucao].var2);
+        case 'S':
+            fscanf(file, "%d %d", (*inst)[ordemInstrucao].var1, (*inst)[ordemInstrucao].var2);
+        case 'B':
+            //TO_DO
+        case 'T':
+            //TO_DO
+        case 'F':
+            fscanf(file, "%d", (*inst)[ordemInstrucao].var1);
+        case 'R':
+            fscanf(file, "%s", (*inst)[ordemInstrucao].arqv);
+        default:
+            break;
+        }
+    }
+    fclose(file);
 }
 
 void lerArquivoProcessoS(instrucao *inst, char *nomeArquivo){
