51255960ed1e1a2a223f523bfc8e1dcd5b084333
JhonatMiranda <jhoncosta1345@gmail.com>
2022-06-21 21:31:08 -0300

    up

--- src/estruturas/leitor.c
+++ src/estruturas/leitor.c
@@ -4,7 +4,7 @@ void lerTerminal(Pipe *p)
 {
     printf("\n PIPE \n");
     char c = ' ';
-    printf("Digite os comandos, um a um (Ex: U ou  L ou I ou M): ");
+    printf("\nDigite os comandos, um a um (Ex: U ou  L ou I ou M): ");
     while (c != 'M'){
         scanf(" %c",&c);
         c = toupper(c);
@@ -28,7 +28,7 @@ int leArquivoPipe(Pipe *p, char *nomeArquivo)
     while(fscanf(file, " %c", &c) != EOF){
         fprintf(stderr, "leitor.c - lerArquivoPipe - leu: %c\n", c);
         escrevePipe(p, c);
-        //sleep(1);
+        sleep(1);
     }
     fclose(file);
     return 1;
@@ -81,44 +81,3 @@ int leArquivoInstrucao(instrucao **inst, char *nomeArquivo)
     fclose(file);
     return 1;
 }
-
-// void lerArquivoProcessoS(instrucao **inst, char *nomeArquivo)
-// {
-//     FILE *arq;
-//     char c;
-//     int ordemInstrucao = 0;
-//     (*inst) = (instrucao *)malloc(1000 * sizeof(instrucao));
-//     arq = fopen(nomeArquivo, "r");
-//     if (arq == NULL)
-//     {
-//         printf("Erro ao abrir arquivo\n");
-//         exit(1);
-//     }
-//     while ((c = fgetc(arq)) != EOF)
-//     {
-//         c = fgetc(arq);
-//         (*inst)[ordemInstrucao].id = c;
-//         switch (c)
-//         {
-//         case 'N':
-//             fscanf(arq, "%d", &(*inst)[ordemInstrucao].var1);
-//         case 'D':
-//             fscanf(arq, "%d", &(*inst)[ordemInstrucao].var1);
-//         case 'V':
-//             fscanf(arq, "%d %d", &(*inst)[ordemInstrucao].var1, &(*inst)[ordemInstrucao].var2);
-//         case 'A':
-//             fscanf(arq, "%d %d", &(*inst)[ordemInstrucao].var1, &(*inst)[ordemInstrucao].var2);
-//         case 'S':
-//             fscanf(arq, "%d %d", &(*inst)[ordemInstrucao].var1, &(*inst)[ordemInstrucao].var2);
-//         case 'F':
-//             fscanf(arq, "%d", &(*inst)[ordemInstrucao].var1);
-//         case 'R':
-//             fscanf(arq, "%s", (*inst)[ordemInstrucao].arqv);
-//         default:
-//             break;
-//         }
-//         ++ordemInstrucao;
-//         c = fgetc(arq);
-//     }
-//     fclose(arq);
-// }
